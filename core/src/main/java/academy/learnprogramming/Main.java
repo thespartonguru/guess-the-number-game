75b1ef05e58747c1304df5c1aa932596cc6d9ce4
Guru Manglam Siddhe <guru@Gurus-MacBook-Air.local>
2023-09-04 11:52:09 +0530

    gameimpl

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -2,11 +2,24 @@ package academy.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     private static final Logger log= LoggerFactory.getLogger(com.sun.tools.javac.Main.class);
+    private static final String CONFIG_LOCATION="beans.xml";
     public static void main(String[] args) {
-        log.info("cvbnm");
+        // create context(container)
+        ConfigurableApplicationContext context
+                = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        NumberGenerator  numberGenerator = context.getBean("numberGenerator",NumberGenerator.class);
+
+        //call method next()
+        int number =numberGenerator.next();
+        log.info("number {}", number);
+
+        //close context
+        context.close();
     }
 }
